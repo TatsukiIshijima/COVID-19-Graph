3b01bbb74f8713aa7eef5b12212246c238361a60
ishijimaitsuki <tatsuki445@gmail.com>
2020-07-29 21:02:44 +0900

    Initial Commit

---
+++ covid-19-graph/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-19-graph
+//
+//  Created by 石島樹 on 2020/07/29.
+//  Copyright © 2020 石島樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ covid-19-graphTests/covid_19_graphTests.swift
@@ -0,0 +1,34 @@
+//
+//  covid_19_graphTests.swift
+//  covid-19-graphTests
+//
+//  Created by 石島樹 on 2020/07/29.
+//  Copyright © 2020 石島樹. All rights reserved.
+//
+
+import XCTest
+@testable import covid_19_graph
+
+class covid_19_graphTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
