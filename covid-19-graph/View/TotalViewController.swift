0ad2b8865ddd5b4025a9aa4e80f2487cab8dcab1
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-03 10:54:46 +0900

    リスト表示

--- covid-19-graph/View/TotalViewController.swift
+++ covid-19-graph/View/TotalViewController.swift
@@ -5,20 +5,46 @@
 
 import UIKit
 
-class TotalViewController: UIViewController {
+final class TotalViewController: UIViewController {
+    @IBOutlet private weak var totalTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("AppDelegate is nil.")
+        }
+
+        appDelegate.appContainer.covid19Repository.fetchTotal().startWithResult { result in
+            switch result {
+            case let .success(total):
+                print("Success \(total.date)")
+            case let .failure(error):
+                print("Error \(error)")
+            }
+        }
+
+        totalTableView.delegate = self
+        totalTableView.dataSource = self
+        totalTableView.register(UINib(nibName: R.nib.totalTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: R.reuseIdentifier.totalTableViewCell.identifier)
+    }
+}
+
+extension TotalViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
+
+extension TotalViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
 
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let totalTableViewCell = tableView.dequeueReusableCell(withIdentifier: R.nib.totalTableViewCell, for: indexPath) else {
+            fatalError("failed dequeueResuableCell with \(R.nib.totalTableViewCell.identifier)")
+        }
+        return totalTableViewCell
+    }
 }
