07be919b267abc0481b6802f70ec68931dd17b5a
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-03 10:58:26 +0900

    ホームのStoryboardのリネーム

--- covid-19-graph/View/HomeViewController.swift
+++ covid-19-graph/View/HomeViewController.swift
@@ -9,17 +9,6 @@ class HomeViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-
-        appDelegate.appContainer?.covid19Repository.fetchTotal().startWithResult { result in
-            switch result {
-            case let .success(total):
-                print("Success \(total.date)")
-            case let .failure(error):
-                print("Error \(error)")
-            }
-        }
-
         delegate = self
     }
 }
