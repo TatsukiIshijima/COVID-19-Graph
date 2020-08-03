518c295472f7394a3a34da0d3b9fdc1ca9540fda
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-03 14:59:53 +0900

    コメント追加

--- covid-19-graph/API/Response/TotalResponse.swift
+++ covid-19-graph/API/Response/TotalResponse.swift
@@ -6,12 +6,20 @@
 import Foundation
 
 struct TotalResponse: APIResponse {
+    // 日付
     var date: Int
+    // PCR検査実施人数
     var pcr: Int
+    // 入院者数
     var hospitalize: Int
+    // 陽性者数
     var positive: Int
+    // 重症者数
     var severe: Int
+    // 退院者数
     var discharge: Int
+    // 死亡者数
     var death: Int
+    // 確認中
     var symptom_confirming: Int
 }
