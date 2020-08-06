8e2891842c4d1a516b9fa1bb97278cc3af1c62a7
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-06 22:15:20 +0900

    navigationBarの設定

--- covid-19-graph/View/Prefecture/PrefectureViewController.swift
+++ covid-19-graph/View/Prefecture/PrefectureViewController.swift
@@ -8,5 +8,14 @@ import UIKit
 class PrefectureViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // TODO: この辺りはTabBarで使用する画面としてまとめたい
+        navigationController?.navigationBar.barTintColor = UIColor(named: R.color.primaryColor.name)
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.white
+        ]
     }
 }
