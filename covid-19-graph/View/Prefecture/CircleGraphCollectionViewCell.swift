c6786e2b70f793eba60d0680c98107e63faa8a66
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-07 18:04:56 +0900

    お試しコード

--- covid-19-graph/View/Prefecture/CircleGraphCollectionViewCell.swift
+++ covid-19-graph/View/Prefecture/CircleGraphCollectionViewCell.swift
@@ -3,11 +3,20 @@
 //  covid-19-graph
 //
 
+import Charts
 import UIKit
 
 class CircleGraphCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private weak var pieChartView: PieChartView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+
+    private func setPieChartData() {}
+
+    private func setPieChartAppearance() {
+        pieChartView.centerText = "年代"
     }
 }
