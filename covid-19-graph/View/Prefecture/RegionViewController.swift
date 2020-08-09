15a7739b2febfbd75d1af3413a41343b20a325a9
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-09 17:08:27 +0900

    地方表示用のViewController追加

---
+++ covid-19-graph/View/Prefecture/RegionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegionViewController.swift
+//  covid-19-graph
+//
+
+import UIKit
+
+class RegionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TODO: この辺りはTabBarで使用する画面としてまとめたい
+        navigationController?.navigationBar.barTintColor = UIColor(named: R.color.primaryColor.name)
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.white
+        ]
+    }
+}
