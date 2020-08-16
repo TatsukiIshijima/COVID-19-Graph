cb82fb4734bbe0a6993664418aa172caf2aa3d12
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-16 16:19:39 +0900

    差分計算の関数追加

--- Core/UseCase/TodayUseCase.swift
+++ Core/UseCase/TodayUseCase.swift
@@ -37,11 +37,28 @@ public final class TodayUseCase {
         let positives = response.1.history.compactMap { $0.positive }.suffix(30)
         let disCharges = response.1.history.compactMap { $0.discharge }.suffix(30)
         let deathes = response.1.history.compactMap { $0.death }.suffix(30)
-        models.append(TodayModel(num: response.0.pcr, history: Array(pcrs)))
-        models.append(TodayModel(num: response.0.positive, history: Array(positives)))
-        models.append(TodayModel(num: response.0.discharge, history: Array(disCharges)))
-        models.append(TodayModel(num: response.0.death, history: Array(deathes)))
+
+        let pcrDiff = toDiffArray(array: pcrs)
+        let positiveDiff = toDiffArray(array: positives)
+        let disChargeDiff = toDiffArray(array: disCharges)
+        let deathDiff = toDiffArray(array: deathes)
+
+        models.append(TodayModel(num: response.0.pcr, history: pcrDiff))
+        models.append(TodayModel(num: response.0.positive, history: positiveDiff))
+        models.append(TodayModel(num: response.0.discharge, history: disChargeDiff))
+        models.append(TodayModel(num: response.0.death, history: deathDiff))
         return models
+    }
+
+    private func toDiffArray(array: ArraySlice<Int>) -> [Int] {
+        return zip(array.indices, array).map { (index: Int, value: Int) -> Int in
+            if index + 1 >= array.endIndex {
+                return 0
+            }
+            let forwardValue = array[index + 1]
+            let diff = forwardValue - value > 0 ? forwardValue - value : 0
+            return diff
+        }
     }
 }
 
