c9aca452b9fbff9ae4f94024f9fb99109f22a559
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-16 21:56:51 +0900

    usecaseを作成

---
+++ Core/UseCase/AlertMapUseCase.swift
@@ -0,0 +1,35 @@
+//
+//  AlertMapUseCase.swift
+//  Core
+//
+
+public final class AlertMapUseCase {
+    private let alertMapView: AlertMapView
+    private let covid19Repository: COVID19Repository
+
+    public init(alertMapView: AlertMapView, covid19Repository: COVID19Repository) {
+        self.alertMapView = alertMapView
+        self.covid19Repository = covid19Repository
+    }
+
+    public func execute() {
+        alertMapView.showLoading()
+        covid19Repository.fetchPrefecture().startWithResult { [weak self] result in
+            switch result {
+            case let .success(response):
+                print("\(response)")
+                self?.alertMapView.fillPrefectures()
+            case let .failure(error):
+                self?.alertMapView.showError(error: error)
+            }
+            self?.alertMapView.hideLoading()
+        }
+    }
+}
+
+public protocol AlertMapView {
+    func fillPrefectures()
+    func showError(error: Error)
+    func showLoading()
+    func hideLoading()
+}
