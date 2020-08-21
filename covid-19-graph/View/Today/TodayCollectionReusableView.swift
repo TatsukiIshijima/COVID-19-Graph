101b9ab1b6696c49e512350b04d6823ecf154123
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-21 21:43:24 +0900

    ヘッダーのレイアウト作成

---
+++ covid-19-graph/View/Today/TodayCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  TodayCollectionReusableView.swift
+//  covid-19-graph
+//
+
+import UIKit
+
+final class TodayCollectionReusableView: UICollectionReusableView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
