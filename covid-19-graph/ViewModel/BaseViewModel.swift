97b4d7c99cd8d71d53f2346f8e9385ad2e11486e
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-23 15:55:47 +0900

    エラーハンドリング用のErrorViewModel作成

--- covid-19-graph/ViewModel/BaseViewModel.swift
+++ covid-19-graph/ViewModel/BaseViewModel.swift
@@ -3,4 +3,23 @@
 //  covid-19-graph
 //
 
+import Core
+import ReactiveSwift
+
 protocol BaseViewModel {}
+
+class ErrorViewModel: BaseViewModel {
+    let errorMutableProperty: MutableProperty<Error?>
+    let errorProperty: Property<Error?>
+
+    init() {
+        errorMutableProperty = MutableProperty(nil)
+        errorProperty = Property(capturing: errorMutableProperty)
+    }
+}
+
+extension ErrorViewModel: ErrorView {
+    func showError(error: Error) {
+        errorMutableProperty.value = error
+    }
+}
