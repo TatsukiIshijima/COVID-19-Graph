9c53f49a80461fedbec032f928a988a82ae734b9
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-24 23:05:56 +0900

    不要な処理削除

--- covid-19-graph/View/Prefecture/RegionViewController.swift
+++ covid-19-graph/View/Prefecture/RegionViewController.swift
@@ -13,15 +13,6 @@ class RegionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // TODO: この辺りはTabBarで使用する画面としてまとめたい
-        navigationController?.navigationBar.barTintColor = UIColor(named: R.color.primaryColor.name)
-        navigationController?.navigationBar.tintColor = .white
-        // navigationController?.navigationBar.prefersLargeTitles = true
-        // navigationController?.navigationItem.largeTitleDisplayMode = .always
-        navigationController?.navigationBar.titleTextAttributes = [
-            .foregroundColor: UIColor.white
-        ]
-
         if let region = self.region {
             print("\(region)")
         }
