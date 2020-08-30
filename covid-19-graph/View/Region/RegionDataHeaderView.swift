074e5834b007b05a42d1058e6ff84e8dcc5d2fbd
ishijimaitsuki <tatsuki445@gmail.com>
2020-08-30 17:43:16 +0900

    StackViewの幅とLabelの紐付け修正

--- covid-19-graph/View/Region/RegionDataHeaderView.swift
+++ covid-19-graph/View/Region/RegionDataHeaderView.swift
@@ -6,8 +6,15 @@
 import UIKit
 
 final class RegionDataHeaderView: UITableViewHeaderFooterView {
-    @IBOutlet weak var primaryTitle: UILabel!
+    @IBOutlet private weak var primaryText: UILabel!
+    @IBOutlet private weak var secondaryText: UILabel!
+    @IBOutlet weak var tertiaryText: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+
+        primaryText.text = "累計\n感染者"
+        secondaryText.text = "新規\n感染者"
+        tertiaryText.text = "PCR\n検査"
     }
 }
