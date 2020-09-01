c3b3ac27a2097b5c0d68780a1953da841c0fb3f7
ishijimaitsuki <tatsuki445@gmail.com>
2020-09-01 21:47:48 +0900

    北海道地方追加

---
+++ covid-19-graph/View/Region/HokkaidoMapView.swift
@@ -0,0 +1,23 @@
+//
+//  HokkaidoMapView.swift
+//  covid-19-graph
+//
+
+import Foundation
+import UIKit
+
+final class HokkaidoMapView: DrawMapView {
+    private var hokkaidoColor = R.color.dangerColor7()!
+
+    override func draw(_ rect: CGRect) {
+        originalWidth = 230
+        originalHeight = 210
+        super.draw(rect)
+
+        hokkaidoColor.setFill()
+        Hokkaido.hokkaido.path.fill()
+        drawPath(path: Hokkaido.hokkaido.path)
+
+        context?.restoreGState()
+    }
+}
